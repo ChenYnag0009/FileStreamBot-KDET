12d8423992a2cc86e8318af5277f2b8c7df2f447
CHENYANG <168895666+ChenYnag0009@users.noreply.github.com>
2025-04-24 09:51:21 +0700

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -28,7 +28,7 @@ class Telegram:
 
 class Server:
     PORT = int(env.get("PORT", 8480))
-    BIND_ADDRESS = str(env.get("BIND_ADDRESS", "0.0.0.0"))
+    BIND_ADDRESS = str(env.get("BIND_ADDRESS", "file2link.sh-vpn.shop"))
     PING_INTERVAL = int(env.get("PING_INTERVAL", "1200"))
     HAS_SSL = str(env.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
 
